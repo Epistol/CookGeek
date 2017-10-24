d56c3fbe8a1d3741dea27657040fdd2283074757
Epistol <mika63270@live.fr>
2017-10-25 00:05:59 +0200

    menu

--- app/Page.php
+++ app/Page.php
@@ -3,10 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Laravel\Scout\Searchable;
 
 class Page extends Model
 {
+	use Searchable;
 	protected $fillable = ['name', 'content'];
 }
 
