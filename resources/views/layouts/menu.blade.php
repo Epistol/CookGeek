5a0e5fd676b520792ca628c7ac4bbacc2b75e673
Epistol <mika63270@live.fr>
2017-10-25 11:41:14 +0200

    menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,20 +1,7 @@
+{{--
 <nav class="navbar navbar-default navbar-static-top">
     <div class="container">
-        <div class="navbar-header">
 
-            <!-- Collapsed Hamburger -->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                <span class="sr-only">Toggle Navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-
-            <!-- Branding Image -->
-            <a class="navbar-brand logo" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
-        </div>
 
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
@@ -53,3 +40,90 @@
         </div>
     </div>
 </nav>
+--}}
+
+
+<nav class="navbar" role="navigation" aria-label="main navigation">
+    <!-- Collapsed Hamburger -->
+
+<div class="container">
+
+
+    <div class="navbar-brand">
+
+        <a class="navbar-brand logo" href="{{ url('/') }}">
+            {{ config('app.name', 'Laravel') }}
+        </a>
+
+        <button class="button navbar-burger" data-target="navMenu">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+
+
+    </div>
+
+    <div class="navbar-menu" id="navMenu">
+        <div class="navbar-start">
+            <a class="navbar-item">
+                Home
+            </a>
+
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                    Docs
+                </a>
+
+                <div class="navbar-dropdown">
+                    <a class="navbar-item">
+                        Overview
+                    </a>
+                    <a class="navbar-item">
+                        Elements
+                    </a>
+                    <a class="navbar-item">
+                        Components
+                    </a>
+                    <hr class="navbar-divider">
+                    <div class="navbar-item">
+                        Version 0.6.0
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="navbar-end">
+            @guest
+                <a class="navbar-item" href="{{ route('login') }}">Login</a>
+                <a class="navbar-item" href="{{ route('register') }}">Register</a>
+                @else
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a class="navbar-link" >
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <div class="navbar-dropdown">
+
+                                <a class="navbar-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            <div>
+                    </div>
+                    @endguest
+        </div>
+    </div>
+
+</div>
+
+
+
+</nav>
+
