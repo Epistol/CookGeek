2aa0a212ab17892cb20f7e8589ddbb7a90a2a864
Epistol <mika63270@live.fr>
2017-10-26 16:27:10 +0200

    foreach

--- resources/views/recipes/add/type.blade.php
+++ resources/views/recipes/add/type.blade.php
@@ -2,18 +2,26 @@
     <div class="control type-selector cc-selector">
 
 
-        <?php
+		<?php
 
-            $types = array("anime", "art", "cartes", "comics", "gaming", "livres", "musique", "tv")
+		$types = array("anime", "art", "cartes", "comics", "gaming", "livres", "musique", "tv")
 
-        ?>
+		?>
+        <div class="columns is-mobile">
 
-        @foreach ($types as $type)
+
+            @foreach ($types as $key=>$type)
+                @php(++$key)
                 <label class="radio">
                     <input type="radio" id="{{$type}}" name="type">
                     <label class="drinkcard-cc {{$type}}" for="{{$type}}"></label>
                 </label>
-        @endforeach
 
+                @if($key == 4)
+                        </div> <div class="columns is-mobile">
+                @endif
+
+            @endforeach
+        </div>
     </div>
 </div>
