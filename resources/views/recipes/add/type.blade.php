add1eaae87537c1ea7b1cfedf0722897870728c4
Epistol <mika63270@live.fr>
2017-10-26 16:12:26 +0200

    types

--- resources/views/recipes/add/type.blade.php
+++ resources/views/recipes/add/type.blade.php
@@ -1,36 +1,19 @@
 <div class="field">
     <div class="control type-selector cc-selector">
-        <label class="radio">
-            <input type="radio" id="anime" name="type">
-            <label class="drinkcard-cc anime" for="anime"></label>
-        </label>
-        <label class="radio">
-            <input type="radio" id="art" name="type">
-            <label class="drinkcard-cc art" for="art"></label>
-        </label>
-        <label class="radio">
-            <input type="radio" id="cartes" name="type">
-            <label class="drinkcard-cc cartes" for="cartes"></label>
-        </label>
-        <label class="radio">
-            <input type="radio" id="comics" name="type">
-            <label class="drinkcard-cc comics" for="comics"></label>
-        </label>
-        <label class="radio">
-            <input type="radio" id="gaming" name="type">
-            <label class="drinkcard-cc gaming" for="gaming"></label>
-        </label>
-        <label class="radio">
-            <input type="radio" id="livres" name="type">
-            <label class="drinkcard-cc livres" for="livres"></label>
-        </label
-        <label class="radio">
-            <input type="radio" id="musique" name="type">
-            <label class="drinkcard-cc musique" for="musique"></label>
-        </label>
-        <label class="radio">
-            <input type="radio" id="tv" name="type">
-            <label class="drinkcard-cc tv" for="tv"></label>
-        </label>
+
+
+        <?php
+
+            $types = array("anime", "art", "cartes", "comics", "gaming", "livres", "musique", "tv")
+
+        ?>
+
+        @foreach ($types as $type)
+                <label class="radio">
+                    <input type="radio" id="{{$type}}" name="type">
+                    <label class="drinkcard-cc {{$type}}" for="{{$type}}"></label>
+                </label>
+        @endforeach
+
     </div>
 </div>
