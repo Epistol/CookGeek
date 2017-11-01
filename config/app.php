9af4c2f79d11ae2cd3ada1925e6e27e935a6c0c1
Epistol <epistolshow@gmail.com>
2017-11-01 22:01:53 +0100

    revert mongo

--- config/app.php
+++ config/app.php
@@ -163,7 +163,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-	    Jenssegers\Mongodb\Auth\PasswordResetServiceProvider::class,
 
         /*
          * Package Service Providers...
