50585491fda7b65268a289da16deb8393ae95fc1
Epistol <mika63270@live.fr>
2017-11-22 23:19:49 +0100

    step css

--- resources/views/recipes/show/steps.blade.php
+++ resources/views/recipes/show/steps.blade.php
@@ -1,13 +1,13 @@
 @forelse($steps as $step)
     <div class="step">
         <div class="columns">
-            <div class="column is-1">
+            <div class="column is-1 is-flex-center">
             <span class="step">
                 {{$step->step_number+1}}
             </span>
 
             </div>
-            <div class="column">
+            <div class="column is-lateral">
                 <div class="content">
                     <p>   {{$step->instruction}}</p>
                 </div>
