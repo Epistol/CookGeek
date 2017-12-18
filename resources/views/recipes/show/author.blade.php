090a05f58ec5cbb1f557b28a1bbd2efdbada4e31
Epistol <mika63270@live.fr>
2017-12-18 01:14:29 +0100

    pareil

--- resources/views/recipes/show/author.blade.php
+++ resources/views/recipes/show/author.blade.php
@@ -1,10 +1,8 @@
-Auteur :
+@lang("common.by")
 
 <?php
 
 $id_auteur = $recette->id_user;
 $nom = DB::table('users')->where('id', $id_auteur)->value('name');
-
-
         ?>
-<a href="/user/{{$nom}}">{{$nom}}</a>
+<a href="/user/{{$nom}}">&nbsp;{{$nom}}</a>
--- resources/views/search/recipe_name.blade.php
+++ resources/views/search/recipe_name.blade.php
@@ -7,31 +7,97 @@
 
         <div class="columns" style="margin-top: 2rem; margin-bottom: 2rem;">
 
-        @foreach ($recettechunk as $recette)
-            <?php
-            $firstimg = DB::table('recipe_imgs')
-                ->where('recipe_id', '=', $recette->id)
-                ->where('user_id', '=', $recette->id_user)
-                ->first();
-            ?>
+            @foreach ($recettechunk as $recette)
+                <?php
+                $firstimg = DB::table('recipe_imgs')
+                    ->where('recipe_id', '=', $recette->id)
+                    ->where('user_id', '=', $recette->id_user)
+                    ->first();
 
-                <div class="column is-6 is-result">
+                $stars = DB::table('recipe_likes')
+                    ->where('id_recipe', '=', $recette->id)
+                    ->avg('note');
+                $stars = number_format($stars, 1, '.', '');
+                $stars =  explode('.', $stars, 2);
+
+                ?>
+
+                <div class="column is-6 is-result" style="max-height: 190px;">
                     <div class="columns">
                         <div class="column is-4 is-paddingless is-marginless">
                             <figure class="image is-1by1" >
                                 <img src="/recipes/{{$recette->id}}/{{$recette->id_user}}/{{$firstimg->image_name}}">
                             </figure>
                         </div>
-                        <div class="column ">
-                            {{$recette->title}}
+                        <div class="column is-7">
+                            <div class="top is-flex">
+                                <h2 class="title">
+                                    {{$recette->title}}
+                                </h2>
+
+
+                            </div>
+                            <div class="middle">
+                                {{-- Auteur / Ingredients--}}
+
+                                <?php
+                                $ingredients =  DB::table('recipes_ingredients')
+                                    ->where('id_recipe', '=', $recette->id)
+                                    ->get();
+                                ?>
+                                <p><b>@lang("recipe.ingredients") : </b>
+                                @foreach($ingredients as $index=>$in)
+                                    <?php
+                                    $nom_in = DB::table('ingredients')->where('id', $in->id_ingredient)->value('name');
+                                    ?>
+                                    @if($loop->last)
+                                        {{$nom_in}}
+                                    @else
+                                        {{$nom_in}},
+                                    @endif
+                                @endforeach
+                                </p>
+
+
+
+                            </div>
+                            <div class="bottom">
+                                <div class="is-flex">
+                                       @include("recipes.show.author")<br />
+                                    <?php
+                                    $type = DB::table('type_recipes')->select('name')->where('id', $recette->id)->first();
+                                    ?>
+                                    <a class="tag" style="margin-left: 0.5rem; margin-right:0.5rem" href="/{{strtolower($type->name)}}">{{$type->name}}</a>   @include("recipes.show.staronly")
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="column is-1 is-marginless is-paddingless">
+                            <div class="top">
+                                <?php
+                                $typeuniv = DB::table('categunivers')
+                                    ->where('id', '=', $recette->type_univers)
+                                    ->first();
+                                ?>
+                                @include("recipes.show.type_univers")
+                            </div>
+                            <div class="middle">
+
+                            </div>
+                            <div class="bottom">
+                               <a class="tag"><i class="material-icons">favorite</i></a>
+                            </div>
+
                         </div>
                     </div>
                 </div>
 
 
-        @endforeach
+            @endforeach
+            <span>
+                 {{ $resultats['recipe']->links() }}
+        </span>
 
-            {{ $resultats['recipe']->links() }}
         </div>
 
 
