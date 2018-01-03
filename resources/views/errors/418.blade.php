8ba8a23a63bb0eb7b96c29da6ff007c11432950b
Epistol <mika63270@live.fr>
2018-01-03 23:13:15 +0100

    'teapot+404'

--- resources/views/errors/418.blade.php
+++ resources/views/errors/418.blade.php
@@ -2,4 +2,12 @@
 @section('titrepage', "Erreur 418 : je suis une théière")
 @section('content')
 
+    <div class="columns" style="display:flex;justify-content:center;align-items:center; ">
+        <div class="column is-half" style="margin-top: 2rem">
+            <div style="width:100%;height:0;padding-bottom:53%;position:relative;"><iframe src="https://giphy.com/embed/n0GsrMHD3jzkA" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/doctor-who-matt-smith-the-n0GsrMHD3jzkA">(GIPHY), Doctor Who, all rights are courtesy of BBC</a></p>
+        </div>
+    </div>
+
+    <p></p>
+
     @endsection
