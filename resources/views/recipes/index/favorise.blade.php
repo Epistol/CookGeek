5e68d49aaaa31404336d2c3c0f08cf8dac41997a
Epistol <mika63270@live.fr>
2018-02-01 16:28:34 +0100

    heartbeat

--- resources/views/recipes/index/favorise.blade.php
+++ resources/views/recipes/index/favorise.blade.php
@@ -1,34 +1,29 @@
 
 <section class="bordered-cdg">
     <div class="columns">
+        {{--Colonne image--}}
         <div class="column">
 
             @empty($heartbeat)
-
             @endempty
 
             @isset($heartbeat->recipe_id)
-
                 <?php
                 $img = DB::table('recipe_imgs')->where('recipe_id', '=', $heartbeat->recipe_id)->first();
                 ?>
-
                 <img src="{{$img}}">
             @endif
 
-
-
             @isset($heartbeat->image_id)
-
             @endif
-
 
             {{--image principale--}}
         </div>
         <div class="column">
             <h1 class="title">Le coup de coeur</h1>
-            <p>Blablabla</p>
-
+            @isset($heartbeat->texte)
+            <p>{{$heartbeat->texte}}</p>
+            @endif
             {{--Fiche info du coup de coeur--}}
         </div>
     </div>
