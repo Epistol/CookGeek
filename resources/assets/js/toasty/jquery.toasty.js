bd1bf4801a52bd60adb14787058e71f89cf32cb4
Epistol <mika63270@live.fr>
2018-04-13 00:27:15 +0200

    fresh

--- resources/assets/js/toasty/jquery.toasty.js
+++ resources/assets/js/toasty/jquery.toasty.js
@@ -5,70 +5,72 @@
  * Released under the MIT license
  */
 
- (function($) {
+(function ($) {
 
- 	//singleton
- 	var singleToasty;
+    //singleton
+    var singleToasty;
 
- 	function Toasty(elem, options){
- 		this.options=options;
- 	}
+    function Toasty(elem, options) {
+        this.options = options;
+    }
 
- 	Toasty.prototype = {
+    Toasty.prototype = {
 
- 		//initialize including neccesary elements in DOM
- 		init: function(){
- 			//Add to dom needed elements
- 			$("body").append('<div id="cdg_t"><img src="'+ this.options.image +'" alt="toasty"></div>');
+        //initialize including neccesary elements in DOM
+        init: function () {
+            //Add to dom needed elements
+            $("body").append('<div id="cdg_t"><img src="' + this.options.image + '" alt="toasty"></div>');
 
-			$("#cdg_t").css({
-                position : 'fixed',
-                right : '-100%',
-                bottom : '0px',
-				display: 'flex',
+            $("#cdg_t").css({
+                position: 'fixed',
+                right: '-100%',
+                bottom: '0px',
+                display: 'flex',
             });
-			if(this.options.sound){
-				$("body").append('<audio id="toasty-audio"><source src="'+ this.options.sound +'" type="audio/mpeg"></source></audio>');
- 			}
- 		},
+            if (this.options.sound) {
+                $("body").append('<audio id="toasty-audio"><source src="' + this.options.sound + '" type="audio/mpeg"></source></audio>');
+            }
+        },
 
- 		pop: function(){
- 			var audio = document.getElementById('toasty-audio');
+        pop: function () {
+            var audio = document.getElementById('toasty-audio');
             audio.volume = 0.5;
-			audio.play();
-			$("#cdg_t").addClass("show-dan");
-			setTimeout( function(){ $("#cdg_t").removeClass("show-dan"); }, 1000);
- 		}
+            audio.play();
+            $("#cdg_t").addClass("show-dan");
+            setTimeout(function () {
+                $("#cdg_t").removeClass("show-dan");
+            }, 1000);
+        }
 
- 	};
+    };
 
- 	$.fn.toasty = function(options) {
+    $.fn.toasty = function (options) {
 
- 		this.each(function(){
-			// Check if we should operate with some method
-			if (/^(pop)$/i.test(options)) {
-				// Normalize method's name
-				options = options.toLowerCase();
-				switch(options){
-					case 'pop':
-						singleToasty.pop();
-						break;
-				}
-			}else if (typeof options == 'object' || !options) {
-				options = $.extend({}, $.fn.toasty.defaults, options);
-				if(!singleToasty){
-					singleToasty = new Toasty($(this), options);
-					singleToasty.init();
-				}
-			}
- 		});
+        this.each(function () {
+            // Check if we should operate with some method
+            if (/^(pop)$/i.test(options)) {
+                // Normalize method's name
+                options = options.toLowerCase();
+                switch (options) {
+                    case 'pop':
+                        singleToasty.pop();
+                        break;
+                }
+            } else if (typeof options == 'object' || !options) {
+                options = $.extend({}, $.fn.toasty.defaults, options);
+                if (!singleToasty) {
+                    singleToasty = new Toasty($(this), options);
+                    singleToasty.init();
+                }
+            }
+        });
 
-	};
+    };
 
-	$.fn.toasty.defaults = {
- 		sound: true,
- 		image: '\\js\\toasty\\toasty.png',
- 		sound: '\\js\\toasty\\toasty.mp3'
- 	};
+    $.fn.toasty.defaults = {
+        sound: true,
+        image: '\\js\\toasty\\toasty.png',
+        sound: '\\js\\toasty\\toasty.mp3'
+    };
 
 })(jQuery);
