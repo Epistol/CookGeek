eaa91fe363dcb31600fe51445e2060f5ae04983d
Epistol <mika63270@live.fr>
2018-05-16 12:02:14 +0200

    social

--- resources/views/recipes/show/social.blade.php
+++ resources/views/recipes/show/social.blade.php
@@ -1,7 +1,7 @@
 <div class="sharing-block">
     <div class="field has-addons" style="margin-left: 1rem">
         <p class="control">
-            <a class="button facebook" @click="showModal = true" >
+                <a class="button facebook" onclick="javascript:window.open('https://www.facebook.com/sharer.php?u={{url()->current()}}&t={{$recette->title}}', '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=500,width=700');return false;" >
                           <span class="icon is-small">
                    <i class="fab fa-facebook-f fa-fw  fa-lg"></i>
 
@@ -11,15 +11,16 @@
 
 
         </p>
+        <?php $univers = DB::table('univers')->where('id', $recette->univers)->first(); ?>
         <p class="control">
-            <a class="button twitter ">
+            <a class="button twitter " onclick="javascript:window.open('https://twitter.com/intent/tweet?text={{$recette->title}}+-+CDG&url={{url()->current()}}&via=CuisineDeGeek&hashtags=cuisine,geek,{{$univers->name}}', '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=500,width=700');return false;">
                           <span class="icon is-small">
                           <i class="fab fa-twitter" aria-hidden="true"></i>
                           </span>
             </a>
         </p>
         <p class="control">
-            <a class="button pinterest ">
+            <a class="button pinterest "  onclick="javascript:window.open('http://pinterest.com/pin/create/button/?url={{url()->current()}}&description={{$recette->title}}', '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=500,width=700');return false;" >
                           <span class="icon is-medium 	 ">
                       <i class="fab fa-pinterest-p fa-fw"></i>
 
@@ -27,7 +28,8 @@
             </a>
         </p>
         <p class="control">
-            <a class="button tumblr " href="http://tumblr.com/widgets/share/tool?canonicalUrl={{url()->full()}}">
+            <a class="button tumblr "onclick="javascript:window.open('http://tumblr.com/widgets/share/tool?canonicalUrl={{url()->current()}}', '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=500,width=700');return false;"
+           >
                           <span class="icon is-small">
                           <i class="fab fa-tumblr fa-fw" aria-hidden="true"></i>
 
@@ -36,7 +38,3 @@
         </p>
     </div>
 </div>
-<modal v-if="showModal" @close="showModal = false" v-cloak>
-    <h3 slot="header">Partage Facebook</h3>
-    <p slot="body"><iframe src="https://www.facebook.com/sharer/sharer.php?u={{url()->current()}}"></iframe> </p>
-</modal>
