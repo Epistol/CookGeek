9fbafcb3c56fde954732f6554f8b7c01246a8f1c
Epistol <mika63270@live.fr>
2018-06-05 12:29:04 +0200

    mail_init_changes

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -51,8 +51,7 @@ Regards,<br>{{ config('app.name') }}
 {{-- Subcopy --}}
 @isset($actionText)
 @component('mail::subcopy')
-If you’re having trouble clicking the "{{ $actionText }}" button, copy and paste the URL below
-into your web browser: [{{ $actionUrl }}]({{ $actionUrl }})
+Si vous avez un problème avec l'action du bouton  "{{ $actionText }}", copiez-collez cet url dans votre navigateur : [{{ $actionUrl }}]({{ $actionUrl }})
 @endcomponent
 @endisset
 @endcomponent
