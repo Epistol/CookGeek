d388ac9b7b57f4298391180886bc7005b0c6935d
Epistol <epistolshow@gmail.com>
2019-01-27 01:31:26 +0100

    Pinterest correction

--- resources/views/recipes/show/social.blade.php
+++ resources/views/recipes/show/social.blade.php
@@ -31,8 +31,8 @@
         @if(count($validPictures) === 1 )
             <p class="control">
                 <a class="button pinterest "
-                   href="https://www.pinterest.com/pin/create/button/?url={{url()->current()}}&media={{strip_tags(clean(collect($validPictures->first())->firstWhere('name', 'normal')['url']))}}&description={{strip_tags(clean($recette->title)) }}">
-                          <span class="icon is-medium 	 ">
+                   href="https://www.pinterest.com/pin/create/button/?url={{url()->current()}}&media={{strip_tags(clean(collect($validPictures->first()->urls)->firstWhere('name', 'normal')['url']))}}&description={{strip_tags(clean($recette->title)) }}">
+                          <span class="icon is-medium">
                       <i class="fab fa-pinterest-p fa-fw"></i>
                                <span hidden>Partager sur Pinterest</span>
                           </span>
