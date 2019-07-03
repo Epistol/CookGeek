37ac5fa04aaf93b4b7c6b45bbc6d1410b5bd1ce4
Epistol <epistolshow@gmail.com>
2019-07-04 00:40:29 +0200

    mobile wip

--- resources/views/layouts/menu/mobile.blade.php
+++ resources/views/layouts/menu/mobile.blade.php
@@ -1,8 +1,6 @@
 <div class="navbar mobile-only" role="navigation" aria-label="main navigation">
     <!-- Collapsed Hamburger -->
     <div class="columns is-mobile is-marginless is-paddingless is-multiline">
-
-        <div class="column is-6">
             {{--Logo--}}
             <div class="navbar-brand">
                 <a href="{{url('/')}}" class="navbar-item is-paddingless is-marginless" style="width: 4.2rem;">
@@ -16,8 +14,6 @@
                 </span>
                 </a>
             </div>
-        </div>
-        <div class="column is-6">
             {{--User--}}
             @guest
                 <div class="navbar-item"><a class="button"
@@ -25,41 +21,6 @@
                 <div class="navbar-item"><a class="button is-primary"
                                             href="{{ route('login') }}">{{__('common.login')}}</a>
                 </div>
-                {{--<div class="navbar-item" ><a class="button is-primary"  @click="showModal = true" >Connexion</a></div>--}}
-                {{--@include("auth.modal.login")--}}
-            @else
-                <div class="navbar-item is-hoverable"
-                     style="display:flex;justify-content:center;align-items:flex-end;    padding-left: 1rem!important;">
-                    <a class="navbar-item user_profile">
-                        <div class="user_picture">
-                            @if(Auth::user()->avatarUser!== 'users/default.png')
-                                <figure class="image is-32x32">
-                                    <img class="is-rounded "
-                                         src="{{Auth::user()->avatarUser}}"
-                                         style="height: 100%;width: 100%;">
-                                </figure>
-                            @else
-                                <figure class="image is-32x32">
-                                    <img class="is-rounded"
-                                         src="https://api.adorable.io/avatars/32/{{ strip_tags(clean(Auth::user()->name))}}">
-                                </figure>
-                            @endif
-
-                        </div>
-                        <a class="navbar-item bg-white">
-                            {{ str_limit(strip_tags(clean(Auth::user()->name, 25, "..."))) }}
-                        </a>
-                    </a>
-                </div>
             @endguest
-        </div>
-
-        <div class="column">
-            {{--Search icon if not shown--}}
-            @if(Route::currentRouteNamed('home'))
-            @else
-            @endif
-            {{--<switch-light></switch-light>--}}
-        </div>
     </div>
 </div>
--- resources/views/layouts/menuMobile.blade.php
+++ resources/views/layouts/menuMobile.blade.php
@@ -1,23 +1,33 @@
 <div class="is-fixed-bottom mobile-only">
-    <div class="column is-12 is-marginless is-paddingless" style="display:flex;    padding-left: 1rem!important;">
+    <nav class="navbar mobile-bottom-menu" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item bg-white" href="{{ route('home') }}">
+                <i class="fas fa-home"></i>
+            </a>
+            <a class="navbar-item bg-white" href="{{ route('recipe.index') }}">
+                <i class="fas fa-utensils"></i>
+            </a>
+            <a class="navbar-item bg-white" href="{{ route('recipe.index') }}">
+                <i class="far fa-treasure-chest"></i>
+            </a>
+            <a class="navbar-item bg-white" href="{{route('page.shop')}}">
+                <i class="fas fa-shopping-cart"></i>
+            </a>
+            <div class="user_picture">
+                @if(Auth::user()->avatarUser !== '')
+                    <figure class="image is-32x32">
+                        <img class="is-rounded test"
+                             src="{{cleanInput(Auth::user()->avatarUser)}}"
+                             style="height: 100%;width: 100%;">
+                    </figure>
+                @else
+                    <figure class="image is-32x32">
+                        <img class="is-rounded"
+                             src="https://api.adorable.io/avatars/32/{{Auth::user()->name}}">
+                    </figure>
+                @endif
+            </div>
+        </div>
+    </nav>
 
-        {{--// Home--}}
-        <a class="navbar-item bg-white" href="{{ route('home') }}">
-            <i class="fas fa-home"></i>
-        </a>
-        {{--// Recipes--}}
-        <a class="navbar-item bg-white" href="{{ route('recipe.index') }}">
-            <i class="fas fa-utensils"></i>
-        </a>
-        {{--// Universes--}}
-        <a class="navbar-item bg-white" href="{{ route('recipe.index') }}">
-            <i class="far fa-treasure-chest"></i>
-        </a>
-
-        <a class="navbar-item bg-white" href="{{route('page.shop')}}">
-            <i class="fas fa-shopping-cart"></i>
-        </a>
-
-
-    </div>
 </div>
